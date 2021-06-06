8dc56430e7d9cc497c2b4517abff80e2855e281f
Kristi Miller <kristi.ann.miller@gmail.com>
2021-06-06 09:37:31 -0600

    Add refineResults function to clean up data upon retrieval

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -6,17 +6,51 @@ import { getSearchResults } from '../../apiCalls';
 import { useState } from 'react';
 
 
-
+type Repo = {
+  name: string,
+  fullName: string,
+  ownerName: string,
+  ownerIcon: string,
+  ownerUrl: string,
+  repoUrl: string,
+  description: string,
+  langage: string,
+  stars: number,
+  forks: number,
+  openIssues: number,
+  created: string,
+  lastUpdated: string
+};
 
 function App() {
 
-  const [searchResults, setSearchResults] = useState('');
+  const [searchResults, setSearchResults] = useState<Repo[]>([]);
 
 
   const handleNewSearch = (search) => {
     getSearchResults(search)
     .then(response => response.data.items)
-    .then(data => setSearchResults(data))
+    .then(data => setSearchResults(refineResults(data)))
+  }
+
+  const refineResults = (results) => {
+    return results.map(repo => {
+      return {
+        name: repo.name,
+        fullName: repo.full_name,
+        ownerName: repo.owner.login,
+        ownerIcon: repo.owner.avatar_url,
+        ownerUrl: repo.owner.url,
+        repoUrl: repo.html_url,
+        description: repo.description,
+        langage: repo.language,
+        stars: repo.stargazers_count,
+        forks: repo.forks,
+        openIssues: repo.open_issues,
+        created: repo.created_at,
+        lastUpdated: repo.updated_at
+      }
+    })
   }
 
   return (
@@ -25,6 +59,7 @@ function App() {
         <img className="logo" src={logo}/>
         <Search
           handleNewSearch={handleNewSearch}
+          searchResults={searchResults}
         />
       </header>
     </div>
