745571e82ed2d924d5e11134e1f74e9d14f6da5a
Kristi Miller <kristi.ann.miller@gmail.com>
2021-06-06 00:40:38 -0600

    Add handleNewSearch function

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,14 +2,30 @@ import React from 'react';
 import Search from '../Search/Search';
 import './App.css';
 import logo from '../../assets/logo.png';
+import { getSearchResults } from '../../apiCalls';
+import { useState } from 'react';
+
+
 
 
 function App() {
+
+  const [searchResults, setSearchResults] = useState('');
+
+
+  const handleNewSearch = (search) => {
+    getSearchResults(search)
+    .then(response => response.data.items)
+    .then(data => setSearchResults(data))
+  }
+
   return (
     <div className="app">
       <header className="app-header">
         <img className="logo" src={logo}/>
-        <Search />
+        <Search
+          handleNewSearch={handleNewSearch}
+        />
       </header>
     </div>
   );
