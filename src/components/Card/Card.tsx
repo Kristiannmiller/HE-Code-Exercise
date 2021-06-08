aed3b3bd963a578e98a91b01763c642a155f5036
Kristi Miller <kristi.ann.miller@gmail.com>
2021-06-08 01:12:36 -0600

    Clean-up code

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -15,14 +15,17 @@ const Card: React.FC<Props> = ({
 
   return (
     <Link className="card" onClick={() => selectRepo(repoData.key)} to={`/${repoData.key}/${repoData.name}`} data-testid={`${repoData.name}-link`}>
-      <section className="card-header">
+
+      <header className="card-header">
         <img className="card-avatar" src={repoData.ownerIcon} alt={`${repoData.ownerName}'s GitHub avatar`}/>
         <section className="card-badges">
           {repoData.language && <div className="card-language">{repoData.language}</div>}
           <div className="card-stars">{`⭑ ${repoData.stars}`}</div>
         </section>
-      </section>
+      </header>
+      
       <h1 className="card-title">{repoData.name}</h1>
+
       <section className="description-wrap">
         {repoData.description && <p className="card-description">{repoData.description}</p>}
         {!repoData.description && <p className="card-description">No Description Available</p>}
